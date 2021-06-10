1cbc0c521305cc65992656292c899c5365ffa2d5
Sean Phillips <seanphillips@Seans-Air.broadband>
2021-06-10 17:25:42 +0100

    updating game class and refactoring

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -4,4 +4,12 @@ feature 'Attacking' do
     click_link 'Attack'
     expect(page).to have_content 'Sean attacked Tarun'
   end
+
+  scenario 'reduce Player 2 HP by 10' do
+    sign_in_and_play
+    click_link 'Attack'
+    click_link 'OK'
+    expect(page).not_to have_content 'Tarun: 60HP'
+    expect(page).to have_content 'Tarun: 50HP'
+  end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,10 +2,23 @@ require 'player'
 
 describe Player do
   subject(:sean) { Player.new('Sean') }
+  subject(:tarun) { Player.new('Tarun') }
 
   describe '#name' do
     it 'returns the name' do
       expect(sean.name).to eq 'Sean'
+    end
+  end
+
+  describe '#hit_points' do
+    it 'returns the hit points' do
+      expect(sean.hit_points).to eq described_class::DEFAULT_HIT_POINTS
+    end
+  end
+
+  describe '#receive_damage' do
+    it 'reduces the player hit points' do
+      expect { sean.receive_damage }.to change { sean.hit_points }.by(-10)
     end
   end
 end
